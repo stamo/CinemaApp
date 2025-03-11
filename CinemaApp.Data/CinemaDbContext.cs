22e19b04f6f8126da8a36863fde210d4500a4f8b
Стамо Петков <sgpetkov@is-bg.net>
2025-03-11 21:27:50 +0200

    type of key of user changed to GUID

--- CinemaApp.Data/CinemaDbContext.cs
+++ CinemaApp.Data/CinemaDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CinemaApp.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CinemaApp.Data
 {
-    public class CinemaDbContext : IdentityDbContext
+    public class CinemaDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
         public CinemaDbContext(DbContextOptions<CinemaDbContext> options)
             : base(options)
--- CinemaApp/Program.cs
+++ CinemaApp/Program.cs
@@ -1,4 +1,5 @@
 using CinemaApp.Data;
+using CinemaApp.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<CinemaDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<CinemaDbContext>();
 builder.Services.AddControllersWithViews();
 
