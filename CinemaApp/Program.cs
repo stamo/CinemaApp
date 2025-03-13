051a842a46d132d34297658f20159b979780d9ed
Kristiyan Ivanov <kristiqn.ivanov@abv.bg>
2025-03-13 22:20:39 +0200

    Fix for custom ApplicationUser not being used in identity.

--- CinemaApp/Program.cs
+++ CinemaApp/Program.cs
@@ -11,9 +11,15 @@ builder.Services.AddDbContext<CinemaDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+    })
+    .AddRoles<IdentityRole<Guid>>()
     .AddEntityFrameworkStores<CinemaDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
